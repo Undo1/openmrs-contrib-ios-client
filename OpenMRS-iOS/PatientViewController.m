6228af414420058de4a0bf3aa1d392a632ebf88f
Undo1 <parker@erwaysoftware.com>
2014-12-02 09:30:51 -0700

    improve address display

--- OpenMRS-iOS/PatientViewController.m
+++ OpenMRS-iOS/PatientViewController.m
@@ -30,18 +30,22 @@
             self.information = @[@{@"Name" : [self notNil:self.patient.name]},
                                  @{@"Age" : [self notNil:self.patient.age]},
                                  @{@"Gender" : [self notNil:self.patient.gender]},
-                                 @{@"Address Line 1" : [self notNil:self.patient.address1]},
-                                 @{@"Address Line 2" : [self notNil:self.patient.address2]},
-                                 @{@"Address Line 3" : [self notNil:self.patient.address3]},
-                                 @{@"Address Line 4" : [self notNil:self.patient.address4]},
-                                 @{@"Address Line 5" : [self notNil:self.patient.address5]},
-                                 @{@"Address Line 6" : [self notNil:self.patient.address6]}];
+                                 @{@"Birthdate" : [self formatDate:[self notNil:self.patient.birthdate]]},
+                                 @{@"Address" : [self formatPatientAdress:self.patient]}];
             dispatch_async(dispatch_get_main_queue(), ^{
                 [self.tableView reloadData];
                 self.title = self.patient.name;
             });
         }
     }];
+}
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [self tableView:tableView cellForRowAtIndexPath:indexPath];
+    NSString *detail = cell.detailTextLabel.text;
+    
+    CGRect bounding = [detail boundingRectWithSize:CGSizeMake(self.view.frame.size.width, 1000) options:NSStringDrawingUsesLineFragmentOrigin attributes:@{NSFontAttributeName : cell.detailTextLabel.font} context:nil];
+    return MAX(44,bounding.size.height+10);
 }
 -(id)notNil:(id)thing
 {
@@ -50,6 +54,77 @@
         return @"";
     }
     return thing;
+}
+-(NSString *)formatPatientAdress:(MRSPatient *)patient
+{
+    NSString *string = [self notNil:patient.address1];
+    if (![[self notNil:patient.address2] isEqual:@""])
+    {
+        string = [string stringByAppendingString:[NSString stringWithFormat:@"\n%@", patient.address2]];
+    }
+    if (![[self notNil:patient.address3] isEqual:@""])
+    {
+        string = [string stringByAppendingString:[NSString stringWithFormat:@"\n%@", patient.address3]];
+    }
+    if (![[self notNil:patient.address4] isEqual:@""])
+    {
+        string = [string stringByAppendingString:[NSString stringWithFormat:@"\n%@", patient.address4]];
+    }
+    if (![[self notNil:patient.address5] isEqual:@""])
+    {
+        string = [string stringByAppendingString:[NSString stringWithFormat:@"\n%@", patient.address5]];
+    }
+    if (![[self notNil:patient.address6] isEqual:@""])
+    {
+        string = [string stringByAppendingString:[NSString stringWithFormat:@"\n%@", patient.address6]];
+    }
+    if (![[self notNil:patient.cityVillage] isEqual:@""])
+    {
+        string = [string stringByAppendingString:[NSString stringWithFormat:@"\n%@", patient.cityVillage]];
+    }
+    if (![[self notNil:patient.stateProvince] isEqual:@""])
+    {
+        string = [string stringByAppendingString:[NSString stringWithFormat:@"\n%@", patient.stateProvince]];
+    }
+    if (![[self notNil:patient.country] isEqual:@""])
+    {
+        string = [string stringByAppendingString:[NSString stringWithFormat:@"\n%@", patient.country]];
+    }
+    if (![[self notNil:patient.postalCode] isEqual:@""])
+    {
+        string = [string stringByAppendingString:[NSString stringWithFormat:@"\n%@", patient.postalCode]];
+    }
+    return string;
+}
+-(NSString *)formatDate:(NSString *)date
+{
+    if (date == nil)
+    {
+        return @"";
+    }
+    
+    NSDateFormatter *stringToDateFormatter = [[NSDateFormatter alloc] init];
+    [stringToDateFormatter setDateFormat:@"Y-MM-dd'T'HH:mm:ss.SSS-Z"];
+    NSDate *newDate = [stringToDateFormatter dateFromString:date];
+    
+//    struct tm  sometime;
+//    const char *formatString = "%Y-%m-%d'T'%H:%M:%S%Z";
+//    (void) strptime_l(date, formatString, &sometime, NULL);
+    
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    formatter.dateStyle = NSDateFormatterShortStyle;
+    formatter.timeStyle = NSDateFormatterNoStyle;
+    
+    NSString *string = [formatter stringFromDate:newDate];
+    
+    if (string == nil)
+    {
+        return @"";
+    }
+    else
+    {
+        return string;
+    }
 }
 -(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
@@ -73,6 +148,8 @@
     
     cell.textLabel.text = key;
     cell.detailTextLabel.text = value;
+    cell.detailTextLabel.numberOfLines = 0;
+    cell.detailTextLabel.backgroundColor = [UIColor redColor];
     
     return cell;
 }
