b974f42365e6ee5a95b0772f2b9f14e644b84f31
Undo1 <parker@erwaysoftware.com>
2014-12-09 07:46:34 -0700

    Cancel previous search operations so that we don't get a race condition on slow networks

--- OpenMRS-iOS/AppDelegate.h
+++ OpenMRS-iOS/AppDelegate.h
@@ -6,11 +6,12 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "AFNetworking.h"
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
-
+@property (nonatomic, strong) AFHTTPRequestOperation *currentSearchOperation;
 
 @end
 
--- OpenMRS-iOS/OpenMRSAPIManager.m
+++ OpenMRS-iOS/OpenMRSAPIManager.m
@@ -88,6 +88,8 @@
 }
 + (void)getPatientListWithSearch:(NSString *)search completion:(void (^)(NSError *error, NSArray *patients))completion
 {
+    AppDelegate *delegate = [[UIApplication sharedApplication] delegate];
+    
     KeychainItemWrapper *wrapper = [[KeychainItemWrapper alloc] initWithIdentifier:@"OpenMRS-iOS" accessGroup:nil];
     NSString *host = [wrapper objectForKey:(__bridge id)(kSecAttrService)];
     NSURL *hostUrl = [NSURL URLWithString:host];
@@ -96,7 +98,9 @@
     
     [[CredentialsLayer sharedManagerWithHost:hostUrl.host] setUsername:username andPassword:password];
     
-    [[CredentialsLayer sharedManagerWithHost:hostUrl.host] GET:[NSString stringWithFormat:@"%@/ws/rest/v1/patient?q=%@", host, [search stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]] parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+    [self cancelPreviousSearchOperations];
+    
+    delegate.currentSearchOperation = [[CredentialsLayer sharedManagerWithHost:hostUrl.host] GET:[NSString stringWithFormat:@"%@/ws/rest/v1/patient?q=%@", host, [search stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]] parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
 //        NSLog(@"Success, %@", [[NSString alloc] initWithData:operation.responseData encoding:NSUTF8StringEncoding]);
         NSDictionary *results = [NSJSONSerialization JSONObjectWithData:operation.responseData options:kNilOptions error:nil];
 //        NSLog(@"array: %@", results[@"results"]);
@@ -197,5 +201,10 @@
     }
     
     [OpenMRSAPIManager presentLoginController];
+}
++ (void)cancelPreviousSearchOperations
+{
+    AppDelegate *delegate = [[UIApplication sharedApplication] delegate];
+    [delegate.currentSearchOperation cancel];
 }
 @end
