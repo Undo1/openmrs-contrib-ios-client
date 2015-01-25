ac44f9ef2311883a99ada7450a8fd480ae72fb8d
Undo1 <parker@erwaysoftware.com>
2015-01-25 14:41:56 -0700

    login unit tests

--- OpenMRS-iOSTests/OpenMRS_iOSTests.m
+++ OpenMRS-iOSTests/OpenMRS_iOSTests.m
@@ -8,6 +8,7 @@
 
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
+#import "OpenMRSAPIManager.h"
 
 @interface OpenMRS_iOSTests : XCTestCase
 
@@ -25,16 +26,64 @@
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    XCTAssert(YES, @"Pass");
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
+- (void)testAdminLogin {
+    [OpenMRSAPIManager logout];
+    
+    XCTestExpectation *expectation = [self expectationWithDescription:@"login"];
+    
+    [OpenMRSAPIManager verifyCredentialsWithUsername:@"admin" password:@"Admin123" host:@"http://demo.openmrs.org/openmrs" completion:^(BOOL success) {
+        XCTAssert(success);
+        
+        [expectation fulfill];
+    }];
+    
+    [self waitForExpectationsWithTimeout:5 handler:^(NSError *error) {
+        XCTAssertNil(error);
     }];
 }
-
+- (void)testNurseLogin {
+    [OpenMRSAPIManager logout];
+    
+    XCTestExpectation *expectation = [self expectationWithDescription:@"login"];
+    
+    [OpenMRSAPIManager verifyCredentialsWithUsername:@"nurse" password:@"Nurse123" host:@"http://demo.openmrs.org/openmrs" completion:^(BOOL success) {
+        XCTAssert(success);
+        
+        [expectation fulfill];
+    }];
+    
+    [self waitForExpectationsWithTimeout:5 handler:^(NSError *error) {
+        XCTAssertNil(error);
+    }];
+}
+- (void)testClerkLogin {
+    [OpenMRSAPIManager logout];
+    
+    XCTestExpectation *expectation = [self expectationWithDescription:@"login"];
+    
+    [OpenMRSAPIManager verifyCredentialsWithUsername:@"clerk" password:@"Clerk123" host:@"http://demo.openmrs.org/openmrs" completion:^(BOOL success) {
+        XCTAssert(success);
+        
+        [expectation fulfill];
+    }];
+    
+    [self waitForExpectationsWithTimeout:5 handler:^(NSError *error) {
+        XCTAssertNil(error);
+    }];
+}
+- (void)testDoctorLogin {
+    [OpenMRSAPIManager logout];
+    
+    XCTestExpectation *expectation = [self expectationWithDescription:@"login"];
+    
+    [OpenMRSAPIManager verifyCredentialsWithUsername:@"doctor" password:@"Doctor123" host:@"http://demo.openmrs.org/openmrs" completion:^(BOOL success) {
+        XCTAssert(success);
+        
+        [expectation fulfill];
+    }];
+    
+    [self waitForExpectationsWithTimeout:5 handler:^(NSError *error) {
+        XCTAssertNil(error);
+    }];
+}
 @end
