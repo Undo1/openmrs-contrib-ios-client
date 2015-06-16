da871726a2830fb4b45211c83c9e81e7b973c091
Yousef Hamza <jo.adam.93@gmail.com>
2015-06-16 17:32:01 +0200

    Add different tabs for online/offline
    add offline and online tabs and switching
    between them with segment control also
    that updated the search method on OpenMRSAPIManager
    
    Signed-off-by: Yousef Hamza <jo.adam.93@gmail.com>

--- OpenMRS-iOS/PatientSearchViewController.m
+++ OpenMRS-iOS/PatientSearchViewController.m
@@ -14,54 +14,92 @@
 
 @interface PatientSearchViewController ()
 
-    @property (atomic, assign) BOOL searchButtonPressed;
+@property (atomic, assign) BOOL searchButtonPressed;
+@property (nonatomic) BOOL isOnline;
+@property (nonatomic, strong) UISegmentedControl *onlineOrOffile;
+@property (nonatomic, strong) UISearchBar *bar;
 
 @end
 
 @implementation PatientSearchViewController
+
 - (void)viewDidLoad
 {
+    self.isOnline = YES;
     self.title = @"Patients";
     [super viewDidLoad];
     [self reloadDataForSearch:@""];
-    UISearchBar *bar = [[UISearchBar alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 64)];
-    bar.autoresizingMask = UIViewAutoresizingFlexibleWidth;
-    bar.delegate = self;
-    [bar sizeToFit];
-    self.tableView.tableHeaderView = bar;
-    [bar becomeFirstResponder];
+
+    self.bar = [[UISearchBar alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 64)];
+    self.bar .autoresizingMask = UIViewAutoresizingFlexibleWidth;
+    self.bar .delegate = self;
+    [self.bar  sizeToFit];
+
+    self.onlineOrOffile = [[UISegmentedControl alloc] initWithItems:@[@"Online", @"Offline"]];
+    self.onlineOrOffile.selectedSegmentIndex = 0;
+    [self.onlineOrOffile addTarget:self action:@selector(switchOnline) forControlEvents:UIControlEventValueChanged];
+    
+    UIView *headerView = [[UIView alloc] init];
+
+    CGFloat width = [[UIScreen mainScreen] bounds].size.width;
+    CGFloat segmentHeight = 33;
+    CGFloat segmentWidth = 250;
+    CGFloat height = 44;
+    [headerView setFrame:CGRectMake(0, 0, width, 88)];
+    [self.onlineOrOffile setFrame:CGRectMake((width-segmentWidth)/2.0, 44+((height-segmentHeight)/2), segmentWidth, segmentHeight)];
+    self.onlineOrOffile.autoresizingMask = UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleLeftMargin;
+    
+    [headerView addSubview:self.onlineOrOffile];
+    [headerView addSubview:self.bar ];
+    self.tableView.tableHeaderView = headerView;
+
+    [self.bar  becomeFirstResponder];
+
     self.searchButtonPressed = NO;
 }
+
 - (void)reloadDataForSearch:(NSString *)search
 {
     if (self.searchButtonPressed) {
         [SVProgressHUD show];
     }
-    [OpenMRSAPIManager getPatientListWithSearch:search completion:^(NSError *error, NSArray *patients) {
-        self.currentSearchResults = patients;
-        dispatch_async(dispatch_get_main_queue(), ^ {
-            if (self.currentSearchResults.count == 0 && self.searchButtonPressed && [SVProgressHUD isVisible])
+    [OpenMRSAPIManager getPatientListWithSearch:search online:self.isOnline completion:^(NSError *error, NSArray *patients) {
+        if (!error) {
+            self.currentSearchResults = patients;
+            dispatch_async(dispatch_get_main_queue(), ^ {
+                if (patients.count == 0 && self.searchButtonPressed && [SVProgressHUD isVisible])
+                {
+                    [SVProgressHUD showErrorWithStatus:@"Couldn't find patients"];
+                    self.searchButtonPressed = NO;
+                }
+                else if (self.searchButtonPressed && [SVProgressHUD isVisible])
+                {
+                    [SVProgressHUD showSuccessWithStatus:[NSString stringWithFormat:@"%lu patients found", self.currentSearchResults.count]];
+                    self.searchButtonPressed = NO;
+                }
+
+                [self.tableView reloadData];
+            });
+        } else {
+            if (self.searchButtonPressed && [SVProgressHUD isVisible])
             {
-                [SVProgressHUD showErrorWithStatus:@"Couldn't find patients"];
-                self.searchButtonPressed = NO;
-            } else if (self.searchButtonPressed && [SVProgressHUD isVisible])
-            {
-                [SVProgressHUD showSuccessWithStatus:[NSString stringWithFormat:@"%lu patients found", self.currentSearchResults.count]];
+                [SVProgressHUD showErrorWithStatus:@"Can't load patients"];
                 self.searchButtonPressed = NO;
             }
-
-            [self.tableView reloadData];
-        });
+        }
     }];
 }
+
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
     return 1;
 }
+
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     return self.currentSearchResults.count;
 }
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
@@ -73,6 +111,7 @@
     cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
     return cell;
 }
+
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     MRSPatient *patient = self.currentSearchResults[indexPath.row];
@@ -80,14 +119,24 @@
     vc.patient = patient;
     [self.navigationController pushViewController:vc animated:YES];
 }
+
 - (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText
 {
     [self reloadDataForSearch:searchText];
 }
+
 - (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar
 {
     self.searchButtonPressed = YES;
     [self reloadDataForSearch:searchBar.text];
     [searchBar resignFirstResponder];
+}
+
+- (void)switchOnline{
+    long index = self.onlineOrOffile.selectedSegmentIndex;
+    self.onlineOrOffile.selectedSegmentIndex = index == 0 ? 0 : 1;
+    self.isOnline = index == 0 ? YES : NO;
+    [self reloadDataForSearch:@""];
+    self.bar.text = @"";
 }
 @end
