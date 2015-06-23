f6ec6372ad28649c757246170c4380f8b84dc62b
Yousef Hamza <jo.adam.93@gmail.com>
2015-06-23 18:17:55 +0200

    Implement NSCoding for Location, Visit, VisitType

--- OpenMRS-iOS/MRSLocation.m
+++ OpenMRS-iOS/MRSLocation.m
@@ -7,7 +7,27 @@
 //
 
 #import "MRSLocation.h"
+#import "MRSHelperFunctions.h"
 
 @implementation MRSLocation
+
+- (id)initWithCoder:(NSCoder *)aDecoder {
+    self = [super init];
+    if (self) {
+        for (NSString *key in [MRSHelperFunctions allPropertyNames:self]) {
+            [self setValue:[aDecoder decodeObjectForKey:key] forKey:key];
+        }
+    }
+    return self;
+}
+
+- (void)encodeWithCoder:(NSCoder *)aCoder {
+    for (NSString *key in [MRSHelperFunctions allPropertyNames:self]) {
+        if (![MRSHelperFunctions isNull:[self valueForKey:key]]) {
+            NSLog(@"key: %@, value: %@", key, [self valueForKey:key]);
+            [aCoder encodeObject:[self valueForKey:key] forKey:key];
+        }
+    }
+}
 
 @end
--- OpenMRS-iOS/MRSVisitType.swift
+++ OpenMRS-iOS/MRSVisitType.swift
@@ -7,12 +7,23 @@
 //
 
 import Foundation
-class MRSVisitType : NSObject
+
+class MRSVisitType : NSObject, NSCoding
 {
     var uuid: String!
     var display: String!
     
     override init() {
         super.init()
+    }
+    required init(coder aDecoder: NSCoder) {
+        super.init()
+        self.uuid = aDecoder.decodeObjectForKey("uuid") as! String
+        self.display = aDecoder.decodeObjectForKey("display") as! String
+    }
+    
+    func encodeWithCoder(aCoder: NSCoder) {
+        [aCoder.encodeObject(self.uuid, forKey: "uuid")]
+        [aCoder.encodeObject(self.display, forKey: "display")]
     }
 }
